2cbd24b70c8863096276a46b139a6c975ed6ed97
vivekkumarkar@gmail.com <vivekkumarkar@gmail.com>
2023-07-25 21:30:25 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+namespace Main_Project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                string path = "E:\\";
+                Console.WriteLine("Enter file name to read out form");
+                string fname = Console.ReadLine();
+                string fpath = path + fname;
+                if (File.Exists(fpath))
+                {
+                    string[] lines = File.ReadAllLines(fpath);
+                    foreach (string line in lines)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+                else { Console.WriteLine("File does not exist"); }
+
+
+            }
+            catch (Exception ex)
+
+            {
+                Console.WriteLine("Error!!!" + ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadKey();
+            }
+        }
+    }
+}
